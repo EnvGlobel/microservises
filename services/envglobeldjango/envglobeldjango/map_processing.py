c3447c6cc30681ebf85e2e347b2f938ae6e77991
Jesus Angel <jesus.angel.9703@gmail.com>
2020-05-30 15:36:24 -0500

    Upgrade function of preprocessing of map due to long execution

--- services/envglobeldjango/envglobeldjango/map_processing.py
+++ services/envglobeldjango/envglobeldjango/map_processing.py
@@ -1,10 +1,12 @@
 from skimage import io
 import numpy as np
+from skimage.util import img_as_uint
+from PIL import Image
 
-COLOR_GREEN = np.array([99, 214, 104, 255])
-COLOR_ORANGE = np.array([255, 151, 77, 255])
-COLOR_RED = np.array([242, 60, 50, 255])
-COLOR_DARK_RED = np.array([129, 31, 31, 255])
+COLOR_GREEN = 6542952
+COLOR_ORANGE = 16750413
+COLOR_RED = 15875122
+COLOR_DARK_RED = 8462111
 
 
 class MapFeatureExtractor:
@@ -13,30 +15,23 @@ class MapFeatureExtractor:
         self.image: np.ndarray
 
     def countFeatures(self):
+        ocurrences = np.bincount(self.image.reshape(-1))
         result = {}
-        result["green"] = 0
-        result["orange"] = 0
-        result["red"] = 0
-        result["darkRed"] = 0
-        for i in range(self.height):
-            for j in range(self.width):
-                if (self.image[i][j] == COLOR_GREEN).all():
-                    result["green"] += 1
-                if (self.image[i][j] == COLOR_ORANGE).all():
-                    result["orange"] += 1
-                if (self.image[i][j] == COLOR_RED).all():
-                    result["red"] += 1
-                if (self.image[i][j] == COLOR_DARK_RED).all():
-                    result["darkRed"] += 1
+        result["green"] = ocurrences[COLOR_GREEN]
+        result["orange"] = ocurrences[COLOR_ORANGE]
+        result["red"] = ocurrences[COLOR_RED]
+        result["darkRed"] = ocurrences[COLOR_DARK_RED]
         return result
 
+    def combineColors(self, image):
+        return image[:, :, 0]*65536+image[:, :, 1]*256+image[:, :, 2]
+
     def extractFromFile(self, path):
-        self.image = io.imread(path)
-        [self.height, self.width, self.colorDepth] = self.image.shape
+        self.image = self.combineColors(io.imread(path))
+        [self.height, self.width] = self.image.shape
         pass
 
 
-# ... or any other NumPy array!
 extractor = MapFeatureExtractor()
 extractor.extractFromFile(r"D:\ws\bk\screenshot\2020_4_25_21_59.png")
 features = extractor.countFeatures()
