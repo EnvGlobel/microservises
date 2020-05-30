a04e076257e405715ebc68341f5704107f40ed60
Freyman Balaguera <freybalaguera@gmail.com>
2020-05-30 12:59:14 -0500

    Changes to map view

--- services/envglobeldjango/frontend/src/components/Predictor.js
+++ services/envglobeldjango/frontend/src/components/Predictor.js
@@ -5,7 +5,7 @@ const Predictor = () => {
     const [viewport, setViewport] = React.useState({ center: [40.819732, -73.948239], zoom: 12 });
 
     return (
-        <Map viewport={viewport} style={{ height: "100%", width: "100%"}}>
+        <Map viewport={viewport} style={{ height: "100%", width: "100%" }}>
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
