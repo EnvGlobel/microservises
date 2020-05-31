4cbe7c1fc6dd13ab6d5470ae2002cf899b91baf2
Jesus Angel <jesus.angel.9703@gmail.com>
2020-05-30 21:18:14 -0500

    Initialized dataProvider for bogota datasets

---
+++ bogota/bogotaDataProvider.py
@@ -0,0 +1,20 @@
+
+import mysql.connector
+
+
+class BogotaDataProvider:
+
+    def __init__(self) -> None:
+
+        self.cnx = mysql.connector.connect(user='root', password='example',
+                                           host='34.67.137.54',
+                                           database='EnvGlobel')
+
+    def saveWeatherData(self, record):
+        cursor = self.cnx.cursor()
+        query = ("INSERT INTO "
+                 "weatherBogota(measureDate, station, temperature, windSpeed, windDirection, precipitation, pressure)"
+                 "VALUES (%(date)s, %(station)s, %(temperature)s, %(windSpeed)s, %(windDirection)s, %(precipitation)s, %(pressure)s)")
+        cursor.execute(query, record)
+        self.cnx.commit()
+        cursor.close()
