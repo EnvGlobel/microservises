e7e719fcfc778c36ab2c26a03eda355cb1d2ac71
Andres Quintero Galvan <aquintero446@gmail.com>
2020-05-31 11:57:01 -0500

    measure pollution

--- services/cubejs/schema/Pollution.js
+++ services/cubejs/schema/Pollution.js
@@ -12,7 +12,55 @@ cube(`Pollution`, {
     count: {
       type: `count`,
       drillMembers: [id, measuredate]
-    }
+    },
+    averageTemp: {
+      sql: `${temp}`,
+      type: `avg`
+    },
+    minTemp: {
+      sql: `${temp}`,
+      type: `min`
+    },
+    maxTemp: {
+      sql: `${temp}`,
+      type: `max`
+    },
+    averageBp: {
+      sql: `${bp}`,
+      type: `avg`
+    },
+    minBp: {
+      sql: `${bp}`,
+      type: `min`
+    },
+    maxBp: {
+      sql: `${bp}`,
+      type: `max`
+    },
+    averageO3: {
+      sql: `${o3}`,
+      type: `avg`
+    },
+    minO3: {
+      sql: `${o3}`,
+      type: `min`
+    },
+    maxO3: {
+      sql: `${o3}`,
+      type: `max`
+    },
+    averageRh: {
+      sql: `${rh}`,
+      type: `avg`
+    },
+    minRh: {
+      sql: `${rh}`,
+      type: `min`
+    },
+    maxRh: {
+      sql: `${rh}`,
+      type: `max`
+    },
   },
   
   dimensions: {
