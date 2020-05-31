299a84e21b3a35583662584a6f3fd80c2877eab4
Andres Quintero Galvan <aquintero446@gmail.com>
2020-05-31 14:23:53 -0500

    basic chart renderer

--- services/envglobeldjango/frontend/src/components/Dashboard.js
+++ services/envglobeldjango/frontend/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import cubejs from '@cubejs-client/core';
 import { Card, CardContent, Grid, Typography } from '@material-ui/core';
 import GraphBar from './graphs/GraphBar';
 import GraphLine from './graphs/GraphLine';
+import ChartRenderer from './graphs/ChartRenderer';
 
 
 const cubejsApi = cubejs(
@@ -14,6 +15,7 @@ const Dashboard = () => {
 
     const graphs = [
         {elem:<GraphLine cubejsApi={cubejsApi} />,xs:12},
+        {elem:<ChartRenderer cubejsApi={cubejsApi} />,xs:12},
         {elem:<GraphBar cubejsApi={cubejsApi} />,xs:4},
         {elem:<GraphBar cubejsApi={cubejsApi} />,xs:4},
         {elem:<GraphBar cubejsApi={cubejsApi} />,xs:4},
