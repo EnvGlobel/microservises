96c60c04caf478e0367e327ee3a0a3b3c3886253
Andres Quintero Galvan <aquintero446@gmail.com>
2020-05-31 07:47:07 -0500

    fix: owner - dashboard_item db relation

--- services/envglobeldjango/dashboard/models.py
+++ services/envglobeldjango/dashboard/models.py
@@ -3,5 +3,5 @@ from dashboard_item.models import DashboardItem
 from django.contrib.auth.models import User
 
 class Dashboard(models.Model):
-    item = models.ManyToManyField(DashboardItem)
+    item = models.ManyToManyField(DashboardItem, blank=True)
     owner = models.ForeignKey(User, related_name='dashboards', on_delete=models.CASCADE, null=True)
